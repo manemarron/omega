193e0545960efe92ba364261dbd94c3a69c7cdb6
Jose Manuel Marron <manemarron@gmail.com>
2015-05-09 00:26:52 -0500

    rest

--- OmegaWeb/src/java/db_ws/DatabaseWSClient.java
+++ OmegaWeb/src/java/db_ws/DatabaseWSClient.java
@@ -5,42 +5,56 @@
  */
 package db_ws;
 
-
-
-import db_ws_client.DatabaseWS;
 import db_ws_client.DatabaseWS_Service;
+import db_ws_client.DatabaseWS;
 
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
-import javax.ws.rs.QueryParam;
 
 /**
+ * REST Web Service
  *
- * @author andermurillo
+ * @author manemarron
  */
-@Path("db/api")
+@Path("api")
 public class DatabaseWSClient {
-    
-//    @Context
-//    private UriInfo context;
-    
-    public DatabaseWSClient(){
-        
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of DatabaseWSClient
+     */
+    public DatabaseWSClient() {
     }
-    
+
+    /**
+     * Retrieves representation of an instance of db_rest_ws.DatabaseWSClient
+     * @return an instance of java.lang.String
+     */
     @GET
     @Produces("text/plain")
+    public String getText() {
+        //TODO return proper representation object
+        return "hola";
+    }
+
+    @PUT
+    @Consumes("application/json")
     @Path("createDatabase")
-    public String createDatabase(@QueryParam("dbName") String dbName, @QueryParam("user") String user, @QueryParam("pw") String pw) {
+    public void createDatabase(CreateDB createDB) {
+        String dbName = createDB.getDbName();
+        String user = createDB.getUser();
+        String pw = createDB.getPw();
+        
         DatabaseWS_Service service = new DatabaseWS_Service();
         DatabaseWS port = service.getDatabaseWSPort();
-        return "" + port.createDatabase(dbName, user, pw);
+        port.createDatabase(dbName, user, pw);
     }
-    
 }
