d9e452978a41e9d3cf68e7cc9bb0472daa5dd200
AnderMZ <elitedragonhunter@hotmail.com>
2015-05-09 14:21:36 -0500

    Ya están implementados todos los servicios web

--- OmegaWeb/src/java/db_ws/DatabaseWS.java
+++ OmegaWeb/src/java/db_ws/DatabaseWS.java
@@ -6,6 +6,7 @@
 package db_ws;
 
 import db.DatabaseAPI;
+import java.util.ArrayList;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -19,9 +20,74 @@ public class DatabaseWS {
 
     @WebMethod(operationName = "createDatabase")
     public boolean createDatabase(@WebParam(name = "dbName") String dbName,
-            @WebParam(name = "user") String user,
-            @WebParam(name = "pw") String pw) {
-        DatabaseAPI dbApi = new DatabaseAPI(); 
+                                  @WebParam(name = "user") String user,
+                                  @WebParam(name = "pw") String pw) {
+        DatabaseAPI dbApi = new DatabaseAPI();
         return dbApi.createDatabase(dbName, user, pw);
+    }
+
+    @WebMethod(operationName = "deleteDatabase")
+    public boolean deleteDatabase(@WebParam(name = "dbName") String dbName,
+                                  @WebParam(name = "user") String user,
+                                  @WebParam(name = "pw") String pw) {
+        DatabaseAPI dbApi = new DatabaseAPI();
+        return dbApi.deleteDatabase(dbName, user, pw);
+    }
+
+    @WebMethod(operationName = "openConnection")
+    public boolean openConnection(@WebParam(name = "dbName") String dbName,
+                                  @WebParam(name = "user") String user,
+                                  @WebParam(name = "pw") String pw) {
+        DatabaseAPI dbApi = new DatabaseAPI();
+        return dbApi.openConnection(dbName, user, pw);
+    }
+
+    @WebMethod(operationName = "closeConnection")
+    public boolean closeConnection(@WebParam(name = "dbName") String dbName,
+                                   @WebParam(name = "user") String user,
+                                   @WebParam(name = "pw") String pw) {
+        DatabaseAPI dbApi = new DatabaseAPI();
+        return dbApi.closeConnection(dbName, user, pw);
+    }
+
+    @WebMethod(operationName = "createTable")
+    public boolean createTable(@WebParam(name = "tableName") String dbName,
+                               @WebParam(name = "columnNames") String[] columnNames,
+                               @WebParam(name = "columnTypes") String[] columnTypes,
+                               @WebParam(name = "nulls") String[] nulls,
+                               @WebParam(name = "pk") String[] pk) {
+        DatabaseAPI dbApi = new DatabaseAPI();
+        return dbApi.createTable(dbName, columnNames, columnTypes, nulls, pk);
+    }
+
+    @WebMethod(operationName = "deleteTable")
+    public boolean deleteTable(@WebParam(name = "tableName") String dbName) {
+        DatabaseAPI dbApi = new DatabaseAPI();
+        return dbApi.deleteTable(dbName);
+    }
+
+    @WebMethod(operationName = "addRow")
+    public boolean addRow(@WebParam(name = "tableName") String dbName,
+                          @WebParam(name = "values") String[] values) {
+        DatabaseAPI dbApi = new DatabaseAPI();
+        return dbApi.addRow(dbName, values);
+    }
+
+    @WebMethod(operationName = "deleteRow")
+    public boolean deleteRow(@WebParam(name = "tableName") String dbName,
+                             @WebParam(name = "columnNames") String[] columnNames,
+                             @WebParam(name = "values") String[] values) {
+        DatabaseAPI dbApi = new DatabaseAPI();
+        return dbApi.deleteRow(dbName, columnNames, values);
+    }
+
+    @WebMethod(operationName = "select")
+    public ArrayList<ArrayList<String>> select(
+            @WebParam(name = "tableName") String dbName,
+            @WebParam(name = "selectColumnNames") String[] selectColumnNames,
+            @WebParam(name = "whereColumnNames") String[] whereColumnNames,
+            @WebParam(name = "values") String[] values) {
+        DatabaseAPI dbApi = new DatabaseAPI();
+        return dbApi.select(dbName, selectColumnNames, whereColumnNames, values);
     }
 }
