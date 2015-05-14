96c8df102eae893ba9d5f1d9f84cc044b06a8061
Jose Manuel Marron <manemarron@gmail.com>
2015-05-14 03:10:43 -0500

    cambios

--- OmegaWeb/src/java/db_ws/models/AddTableModel.java
+++ OmegaWeb/src/java/db_ws/models/AddTableModel.java
@@ -16,8 +16,18 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement(name = "AddTableModel")
 public class AddTableModel {
+    private String table_name;
     private ArrayList<Column> columns;
 
+    public String getTable_name() {
+        return table_name;
+    }
+
+    @XmlElement
+    public void setTable_name(String table_name) {
+        this.table_name = table_name;
+    }
+    
     public ArrayList<Column> getColumns() {
         return columns;
     }
@@ -26,5 +36,43 @@ public class AddTableModel {
     @XmlElementWrapper(name="columns")
     public void setColumns(ArrayList<Column> columns) {
         this.columns = columns;
+    }
+    
+    public ArrayList<String> getColumnNames(){
+        ArrayList<String> names = new ArrayList();
+        for(int i=0;i<columns.size();i++){
+            names.add(columns.get(i).getName());
+        }
+        return names;
+    }
+    
+    public ArrayList<String> getColumnTypes(){
+        ArrayList<String> types = new ArrayList();
+        for(int i=0;i<columns.size();i++){
+            String type = columns.get(i).getType();
+            if(type.contains("char")){
+                type += String.format("(%d)", columns.get(i).getSize());
+            }
+            types.add(type);
+        }
+        return types;
+    }
+    
+    public ArrayList<String> getNulls(){
+        ArrayList<String> nulls = new ArrayList();
+        for(int i=0;i<columns.size();i++){
+            nulls.add(columns.get(i).isNullable()?"NULL":"NOT NULL");
+        }
+        return nulls;
+    }
+    
+    public ArrayList<String> getPks(){
+        ArrayList<String> pks = new ArrayList();
+        for(int i=0;i<columns.size();i++){
+            Column column = columns.get(i);
+            if(column.isPk())
+                pks.add(column.getName());
+        }
+        return pks;
     }
 }
--- OmegaWeb/src/java/users_connection/UsersConnection.java
+++ OmegaWeb/src/java/users_connection/UsersConnection.java
@@ -9,9 +9,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -73,5 +73,47 @@ public class UsersConnection {
             con.close();
             con = null;
         }
+    }
+    
+    public boolean setDatabaseToUser(int user_id, String dbName, String user, String pw) {
+        boolean success = false;
+        try {
+            setConnection();
+            String query = "UPDATE USERS SET DB_NAME=?,DB_USER=?,DB_PASSWORD=? WHERE ID=?";
+            List<?> params = Arrays.asList(new Object[]{dbName, user, pw, user_id});
+            if (executeQuery(query, params) > 0) {
+                success = true;
+            }
+        } catch (SQLException sqlEx) {
+            System.err.println(sqlEx.getMessage());
+        } finally {
+            try {
+                closeConnection();
+            } catch (SQLException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
+        return success;
+    }
+
+    public boolean removeDatabaseFromUser(int user_id) {
+        boolean success = false;
+        try {
+            setConnection();
+            String query = "UPDATE USERS SET DB_NAME=?,DB_USER=?,DB_PASSWORD=? WHERE ID=?";
+            List<?> params = Arrays.asList(new Object[]{null, null, null, user_id});
+            if (executeQuery(query, params) > 0) {
+                success = true;
+            }
+        } catch (SQLException sqlEx) {
+            System.err.println(sqlEx.getMessage());
+        } finally {
+            try {
+                closeConnection();
+            } catch (SQLException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
+        return success;
     }
 }
